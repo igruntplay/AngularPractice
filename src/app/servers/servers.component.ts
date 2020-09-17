8b6f81cabb914a720c11384ae122e5a669fa7fc6
igruntplay <gaspar.onesto@gmail.com>
2020-09-17 14:27:09 -0300

    added btn timer

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,7 +10,12 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
 
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+
+   }
 
   ngOnInit(): void {
   }
