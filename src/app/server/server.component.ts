b8c1ecffba5ae3e231b4a14cc94bdb622b298211
igruntplay <gaspar@eudaimonia.com.ar>
2020-09-21 11:26:18 -0300

    trying to apply css with TS classes ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-server',
-    templateUrl: './server.component.html',
-    //styleUrls: ['./server.component.css']
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `,
+  ],
+  //styleUrls: ['./server.component.css']
 })
-export class ServerComponent{
-    // hardcoded..
-    serverId = 10;
-    serverStatus = 'offline';
-    
-    constructor(){
-        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
-    }
+export class ServerComponent {
+  // hardcoded..
+  serverId = 10;
+  serverStatus = 'offline';
 
-    getServerStatus(){
-        return this.serverStatus;
-    }
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
-    getColor(){
-        return this.serverStatus == 'online' ? 'green' : 'red';
-    }
+  getServerStatus() {
+    return this.serverStatus;
+  }
 
+  getColor() {
+    return this.serverStatus == 'online' ? 'green' : 'red';
+  }
 }
