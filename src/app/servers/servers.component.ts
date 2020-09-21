6fae13140ee30d303d19bfdc4cfcbe146b826a9b
igruntplay <gaspar@eudaimonia.com.ar>
2020-09-21 13:05:20 -0300

    Lists with ngFor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,33 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-
   allowNewServer = false;
-  serverCreationStatus = 'No server was created.'; 
+  serverCreationStatus = 'No server was created.';
   serverName = 'TestServer';
   serverCreated = false;
+  servers = ['TestServer', 'TestServer2'];
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
-    },2000);
-
-   }
-
-  ngOnInit(): void {
+    }, 2000);
   }
 
-  onCreateServer(){
+  ngOnInit(): void {}
+
+  onCreateServer() {
     this.serverCreated = true;
-    this.serverCreationStatus = 'Server was created, the name is: ' + this.serverName;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus =
+      'Server was created, the name is: ' + this.serverName;
   }
 
   // Toma el value del input y lo almacena en una variable
-  onUpdateServerName(event: Event){
+  onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
   }
-
 }
